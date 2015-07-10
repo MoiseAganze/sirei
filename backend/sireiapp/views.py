41cf8c5bd8d87062240bdcb2d712db1730eb81e2
Luis <josuepanzu8@gmail.com>
2015-07-11 01:05:35 +0200

    application de la permission personnaliser sur le model Entrepreneur

--- backend/sireiapp/views.py
+++ backend/sireiapp/views.py
@@ -1,32 +1,13 @@
+from .customizers import EntrepreneurModelPermission
 from .models import Entrepreneur, Investor, Project, Comment, Message
 from .serializer import EntrepreneurSerializer, InvestorSerializer, ProjectSerializer, MessageSerializer, CommentSerializer
-from rest_framework import mixins, permissions, authentication, generics
+from rest_framework import mixins, permissions, authentication, generics, viewsets
 
 from django.contrib.sessions import models
 
-class EntrepreneurAPIView(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    generics.GenericAPIView):
-    
+class EntrepreneurAPIView(viewsets.ModelViewSet):
     queryset = Entrepreneur.objects.all()
     serializer_class = EntrepreneurSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser, EntrepreneurModelPermission(),)
     
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-    def get(self, request, *args, **kwargs ):
-        if kwargs.get("pk"):
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-        
-    
+                
