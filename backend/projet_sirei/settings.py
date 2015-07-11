c4d60caa4f4694fa588e3c1fa581518a41b06be8
Luis <josuepanzu8@gmail.com>
2015-07-11 03:07:41 +0200

    clé secret aléatoire (pour renforcer la sécurité)

--- backend/projet_sirei/settings.py
+++ backend/projet_sirei/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import os
+import uuid
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%if%*_pyek^e5-$$_$n!uj2@jd1ac%hg4y)xu$se0977xy(m&('
+
+os.environ.setdefault('SECRET_KEY', str(uuid.uuid4().hex))
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
