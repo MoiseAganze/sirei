c332ae732e2d3cd11b2071150f65eb23e398dda7
Luis <josuepanzu8@gmail.com>
2024-06-06 00:53:02 +0200

    création des models

--- sireiapp/models.py
+++ sireiapp/models.py
@@ -1,3 +1,28 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class Person (models.Model):
+    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=30)
+    profile = models.CharField(max_length=80)
+    domain = models.CharField(max_length=255)
+
+class Entrepreneur(Person):
+    phone_number = models.CharField(max_length=200)
+    project_file = models.FileField(upload_to="media", blank=True, null=True)
+
+class Investor(Person):
+    experience_year = models.IntegerField(null=False)
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    sector = models.CharField(max_length=255)
+    description = models.TextField(null=False)
+    author = models.ForeignKey(Entrepreneur, on_delete=models.CASCADE)
+    date_published = models.DateTimeField(default=datetime.now())
+
+class Comment(models.Model):
+    content = models.TextField(null=False)
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
+    date_send = models.DateTimeField(default=datetime.now())
