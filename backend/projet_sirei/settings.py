458400bdd31a06c9752d5b776664de15722a5775
Luis <josuepanzu8@gmail.com>
2024-06-26 00:13:21 +0200

    Spécification des chemins pour les fichiers media dans les settings

--- backend/projet_sirei/settings.py
+++ backend/projet_sirei/settings.py
@@ -125,3 +125,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.SessionAuthentication'],
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
